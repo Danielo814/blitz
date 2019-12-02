ee74c53264f2a340a8ba17933bc5cf91b520d276
Danielo814 <ojedadaniel92@gmail.com>
2019-12-02 11:13:58 -0800

    splice instead of index notation

--- phoneNumberMask/script.js
+++ phoneNumberMask/script.js
@@ -3,17 +3,14 @@ let test = document.getElementById("input");
 test.addEventListener(
   "keyup",
   function(evt) {
-    // var n = parseInt(this.value.replace(/\D/g, ""), 10);
-    // test.value = n.toLocaleString();
     let st = this.value.split("");
+    console.log(st);
     if (this.value.length == 4) {
-      //   let st = this.value.split("");
-      st[0] = "(";
-      st[4] = ")";
+      st.splice(0, 0, "(");
+      st.splice(4, 0, ")");
       this.value = st.join("");
-      console.log(this.value.length);
-    } else if (this.value.length > 8) {
-      st[8] = "-";
+    } else if (this.value.length == 8) {
+      st.splice(8, 0, "-");
       this.value = st.join("");
     }
   },
