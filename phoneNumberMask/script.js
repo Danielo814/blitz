8fe4c86c0ed36dba62f5fd01243278a1d13a6e97
Danielo814 <ojedadaniel92@gmail.com>
2019-12-02 09:04:29 -0800

    add phone number formatting

--- phoneNumberMask/script.js
+++ phoneNumberMask/script.js
@@ -1,16 +1,21 @@
-// function test() {
-//   let test = document.getElementById("input");
-//   test.value[0] = "(";
-//   console.log(test.value);
-// }
-
 let test = document.getElementById("input");
 
 test.addEventListener(
   "keyup",
   function(evt) {
-    var n = parseInt(this.value.replace(/\D/g, ""), 10);
-    test.value = n.toLocaleString();
+    // var n = parseInt(this.value.replace(/\D/g, ""), 10);
+    // test.value = n.toLocaleString();
+    let st = this.value.split("");
+    if (this.value.length == 4) {
+      //   let st = this.value.split("");
+      st[0] = "(";
+      st[4] = ")";
+      this.value = st.join("");
+      console.log(this.value.length);
+    } else if (this.value.length > 8) {
+      st[8] = "-";
+      this.value = st.join("");
+    }
   },
   false
 );
