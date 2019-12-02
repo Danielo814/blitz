3c4f970f9831773d205613b7579c6ee385ecd00a
Danielo814 <ojedadaniel92@gmail.com>
2019-12-02 08:48:01 -0800

    testing input with commas using regex

--- phoneNumberMask/script.js
+++ phoneNumberMask/script.js
@@ -1,5 +1,16 @@
-function test() {
-  let test = document.getElementById("input");
-  test.value[0] = "(";
-  console.log(test.value);
-}
+// function test() {
+//   let test = document.getElementById("input");
+//   test.value[0] = "(";
+//   console.log(test.value);
+// }
+
+let test = document.getElementById("input");
+
+test.addEventListener(
+  "keyup",
+  function(evt) {
+    var n = parseInt(this.value.replace(/\D/g, ""), 10);
+    test.value = n.toLocaleString();
+  },
+  false
+);
