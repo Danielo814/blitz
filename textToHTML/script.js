c3d51cf8fb8de887c72d28b15a8e203f91d9c3e0
Danielo814 <ojedadaniel92@gmail.com>
2019-12-06 16:50:31 -0800

    added functionality for inserting text from file to body of html doc

--- textToHTML/script.js
+++ textToHTML/script.js
@@ -2,4 +2,6 @@ fetch("/textFile.txt")
   .then(response => response.text())
   .then(data => {
     console.log(data);
+    let body = document.getElementById("body");
+    body.innerHTML = data;
   });
