6f3e5e83f0a176a6452168dd681c00c909bd2692
Danielo814 <ojedadaniel92@gmail.com>
2019-12-08 11:31:29 -0800

    formats heading elements

--- textToHTML/script.js
+++ textToHTML/script.js
@@ -1,7 +1,22 @@
 fetch("/textFile.txt")
   .then(response => response.text())
   .then(data => {
-    console.log(data);
     let body = document.getElementById("body");
-    body.innerHTML = data;
+    var dataArr = data.split("\n");
+    console.log(dataArr[0][0]);
+    for (let i = 0; i < dataArr.length; i++) {
+      console.log(dataArr[i].slice(1, dataArr[i].length));
+      if (dataArr[i][0] === "#" && dataArr[i][1] !== "#") {
+        body.innerHTML +=
+          "<h1>" + dataArr[i].slice(1, dataArr[i].length) + "</h1>";
+      } else if (dataArr[i].slice(0, 2) === "##" && dataArr[i][2] !== "#") {
+        body.innerHTML +=
+          "<h2>" + dataArr[i].slice(2, dataArr[i].length) + "</h2>";
+      } else if (dataArr[i].slice(0, 3) === "###") {
+        body.innerHTML +=
+          "<h3>" + dataArr[i].slice(3, dataArr[i].length) + "</h3>";
+      } else {
+        body.innerHTML += "<p>" + dataArr[i] + "</p>";
+      }
+    }
   });
