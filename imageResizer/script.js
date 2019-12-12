9da13a66bcb68d7b9015a5ca98b9e07ad95372dc
Danielo814 <ojedadaniel92@gmail.com>
2019-12-11 21:28:15 -0800

    makes a img element, with attribute of link from input box

--- imageResizer/script.js
+++ imageResizer/script.js
@@ -1,4 +1,9 @@
 function imgFunc() {
   let myImage = document.getElementById("imgUrl").value;
+  let newImage = document.createElement("img");
+  newImage.setAttribute("src", `${myImage}`);
+  document.body.append(newImage);
+  console.log(newImage);
+
   console.log(typeof myImage);
 }
