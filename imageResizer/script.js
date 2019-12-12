2d01fb37d396d4558e6ac1641ee13faaa745bfc8
Danielo814 <ojedadaniel92@gmail.com>
2019-12-12 12:44:58 -0800

    add function for assigning height and width

--- imageResizer/script.js
+++ imageResizer/script.js
@@ -1,12 +1,24 @@
-var myImage = document.getElementById("imgUrl");
-myImage.addEventListener("keydown", function(event) {
+var myImageUrl = document.getElementById("imgUrl");
+myImageUrl.addEventListener("keydown", function(event) {
   if (event.keyCode === 13) {
-    createImage(myImage.value);
+    createImage(myImageUrl.value);
   }
 });
 
 function createImage(url) {
   let newImage = document.createElement("img");
   newImage.setAttribute("src", `${url}`);
+  //   assignWidth(newImage);
+  //   assignHeight(newImage);
   document.body.append(newImage);
+}
+
+function assignWidth(image) {
+  let width = document.getElementById("width");
+  image.setAttribute("style", `width: ${width.value}px`);
+}
+
+function assignHeight(image) {
+  let height = document.getElementById("height");
+  image.setAttribute("style", `height: ${height.value}px`);
 }
