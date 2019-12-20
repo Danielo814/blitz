d50ab2145d4c0389caa2483fa407072ba9b88313
Danielo814 <ojedadaniel92@gmail.com>
2019-12-19 20:05:00 -0800

    add temp variable

--- reverse_string_recursion.js
+++ reverse_string_recursion.js
@@ -1,10 +1,15 @@
 function reverseString(s) {
   function swap(left, right) {
+    let temp = "";
     if (left < right) {
+      temp = s[left];
       s[left] = s[right];
-      s[right] = s[left];
-      return swap(left + 1, right - 1);
+      s[right] = temp;
+      swap(left + 1, right - 1);
     }
+    return;
   }
   swap(0, s.length - 1);
 }
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
