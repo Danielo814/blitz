67b1351530f58063235a5f2173b268fef7e70817
Danielo814 <ojedadaniel92@gmail.com>
2020-02-26 20:36:51 -0800

    export default button class

--- translator/src/components/Button.js
+++ translator/src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import LanguageContext from "../context/LanguageContext";
 import ColorContext from "../context/ColorContext";
 
-class Button extends React.Component {
+export default class Button extends React.Component {
   renderSubmit(value) {
     return value === "english" ? "Submit" : "Voorleggen";
   }
