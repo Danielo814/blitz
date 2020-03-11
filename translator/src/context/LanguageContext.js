42d30a471525141ea465486ead791a5c3ec46f8c
Danielo814 <ojedadaniel92@gmail.com>
2020-03-10 22:40:54 -0700

    setstate with callback

--- translator/src/context/LanguageContext.js
+++ translator/src/context/LanguageContext.js
@@ -5,5 +5,7 @@ const Context = React.createContext("english");
 class LanguageStore extends React.Component {
   state = { language: "english" };
 
-  onLanguageChange = language => {};
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
 }
