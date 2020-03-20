4466dfa3e4e8835c5520a01b785d6bad0c2a3b5e
Danielo814 <ojedadaniel92@gmail.com>
2020-03-20 15:36:04 -0700

    solution to reverse an interger

--- concepts/exercises/reverseint/index.js
+++ concepts/exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversed = n.toString().split("");
+  if (n < 0) {
+    reversed.shift();
+  }
+  reversed = parseInt(reversed.reverse().join(""), 10);
+  return n < 0 ? reversed * -1 : reversed;
+}
 
 module.exports = reverseInt;
