2a251b8deda392558a4de80e690a9fed9f686a46
Danielo814 <ojedadaniel92@gmail.com>
2020-04-11 13:47:28 -0700

    connect to instagram

---
+++ connect_to_instagram/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InstagramFeed from "./components/InstagramFeed";
+
+const App = () => {
+  return (
+    <div>
+      <InstagramFeed />
+    </div>
+  );
+};
+
+export default App;
---
+++ connect_to_instagram/src/components/InstagramFeed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InstagramFeed = () => {
+  return <div>this is instagram feed</div>;
+};
+
+export default InstagramFeed;
