4b202d413ab08a5dbaac43e768657da845d90905
Danielo814 <ojedadaniel92@gmail.com>
2020-04-11 14:12:10 -0700

    add state for input text

--- connect_to_instagram/src/components/InstagramFeed.js
+++ connect_to_instagram/src/components/InstagramFeed.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InstagramFeed = () => {
+  const [username, setUsername] = useState("");
+  const updateUsername = (event) => {
+    setUsername(event.target.value);
+  };
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-3">
           <h4>Enter Username:</h4>
-          <input type="text" className="form-control" placeholder="Username" />
+          <input
+            type="text"
+            onChange={(event) => updateUsername(event)}
+            className="form-control"
+            placeholder="Username"
+          />
           <button>Get Instagram Posts</button>
         </div>
         <div className="col-md-9"></div>
