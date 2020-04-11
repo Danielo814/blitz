42c83e0d72e05a7470a174f52ba9873a0449dfe3
Danielo814 <ojedadaniel92@gmail.com>
2020-04-11 13:59:59 -0700

    push

--- connect_to_instagram/src/components/InstagramFeed.js
+++ connect_to_instagram/src/components/InstagramFeed.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const InstagramFeed = () => {
-  return <div>this is instagram feed</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <h4>Enter Username:</h4>
+          <input type="text" className="form-control" placeholder="Username" />
+          <button>Get Instagram Posts</button>
+        </div>
+        <div className="col-md-9"></div>
+      </div>
+    </div>
+  );
 };
 
 export default InstagramFeed;
